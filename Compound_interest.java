c460444db6b3b5633c9225ac309b373e62a35139
Teja9703047204 <102518952+Teja9703047204@users.noreply.github.com>
2022-08-26 04:24:43 +0530

    import java.util.Scanner; class compound {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         double CI = a*(Math.pow((1 + b/ 100),c));         System.out.printf("%.2f",CI);     } }

---
+++ Compound_interest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class compound
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        double CI = a*(Math.pow((1 + b/ 100),c));
+        System.out.printf("%.2f",CI);
+    }
+}
