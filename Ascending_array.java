aa8f28170fc379690e56b257d31ddcfb5c7cb452
Teja9703047204 <102518952+Teja9703047204@users.noreply.github.com>
2022-09-11 17:41:13 +0530

    import java.util.Scanner; class ascend {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         int a[]=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(int i=0;i<n-1;i++)         {             if(a[i]>=a[i+1])             {                 System.out.println("no");                 System.exit(0);             }         }         System.out.println("yes");     } }

---
+++ Ascending_array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class ascend
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(a[i]>=a[i+1])
+            {
+                System.out.println("no");
+                System.exit(0);
+            }
+        }
+        System.out.println("yes");
+    }
+}
